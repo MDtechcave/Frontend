f094edf1209e5629d4af202b72ab72c74596a215
mihle-aviwe-dudumashe <mihleaviwe6@gmail.com>
2026-02-17 11:17:46 +0200

    Modified the project structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,54 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from './components/Login.vue';
+
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+import MealView from '../views/MealView.vue';
+import Register from '../views/Register.vue';
+import Contact from '../views/Contact.vue';
+import Order from '../views/Order.vue';
+
+const routes = [
+  { path: '/login', components: Login },
+  {path: '/Register', components: Register,
+    meta: {requiresAuth: true}
+   },
+  { path: '/',
+     component:NavBar ,
+    meta:{requiresAuth: true}
+  },
+    {path: '/MealView', components: MealView,
+    meta: {requiresAuth: true}
+   },
+  {path: '/GoalView', components: GoalView,
+    meta: {requiresAuth: true}
+   },
+  {path: '/Footer', components: Footer,
+    meta: {requiresAuth: true}
+   },
+    {path: '/Order', components: Order,
+    meta: {requiresAuth: true}
+   },
+     {path: '/Contact', components: Contact,
+    meta: {requiresAuth: true}
+   },
+
+
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const isloggedIn = localStorage.getItem("user");
+
+  if (to.meta.requiresAuth && !isloggedIn) {
+    next('/login'); //sends you to login
+  } else {
+    next(); //allows access
+  }
+});
+
+export default router;
